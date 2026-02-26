9d8f623daeabd3808207fa759f677476de4ce897
Otibra <otienobrian718@gmail.com>
2026-02-26 05:22:38 -0800

    Update project structure and files

---
+++ management_task/task/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Task(models.Model):
+     
+     PRIORITY_CHOICES = [
+        ('LOW', 'Low'),
+        ('MEDIUM', 'Medium'),
+        ('HIGH', 'High'),
+     ]
+
+     STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed'),
+    ]
+     
+     title = models.CharField(max_length=50)
+     description = models.TextField()
+     due_date = models.DateTimeField()
+
+     priority_level = models.CharField(
+        max_length=10,
+        choices=PRIORITY_CHOICES,
+        default='LOW'
+    )
+
+     status = models.CharField(
+        max_length=15,
+        choices=STATUS_CHOICES,
+        default='PENDING'
+    )
+
+     user = models.ForeignKey(User, on_delete = models.CASCADE, name='tasks')
+
+     def __str__(self):
+        return self.title
+
