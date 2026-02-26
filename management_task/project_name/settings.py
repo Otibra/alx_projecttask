21421762f93315851f0613da78239ae40c78d1f2
Otibra <otienobrian718@gmail.com>
2026-02-26 12:27:50 -0800

    update: implement Task CRUD views and serializer

--- management_task/project_name/settings.py
+++ management_task/project_name/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task',
+    'rest_framework',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- management_task/task/views.py
+++ management_task/task/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework import generics
+
+class TaskListView(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskCreateView(generics.CreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskUpdateView(generics.UpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskDeleteView(generics.DestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
 
 # Create your views here.
